44725c4c13822e7be6e0e269927d076fe2581865
Ricky Mormor <rickymormor1@gmail.com>
2018-10-27 11:39:25 +0000

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -10,7 +10,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     //const port = 8000 // Change this to your server port
-    return `https://cwaku.github.io/restaurant-reviews-app/data/restaurants.json`;
+    return `https://cwaku.github.io/Restaurant-reviews-app/data/restaurants.json`;
     //return `http://localhost:${port}/data/restaurants.json`;
   }
 
